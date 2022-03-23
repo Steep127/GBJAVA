8b9c433b2305eed345fe430cdf1f91bdbfedb8db
Steep127 <42293431+Steep127@users.noreply.github.com>
2022-03-23 21:10:48 +0300

    Delete GB16.01.22 directory
    refresh

--- GB16.01.22/src/RU/GB2/LESSON2/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package RU.GB2.LESSON2;
-
-public class MyArraySizeException extends Exception{
-    public MyArraySizeException (String otvet){
-        super(otvet);
-    }
-}
--- GB16.01.22/src/RU/GB2/LESSON3/Fruit.java
+++
@@ -1,4 +0,0 @@
-package RU.GB2.LESSON3;
-
-public class Fruit {
-}
